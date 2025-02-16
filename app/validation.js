f461730f3d2d380fd5978d73db0a34c6b12bab52
JohnGallemore <122250845+JohnGallemore@users.noreply.github.com>
2025-02-15 21:46:44 -0600

    Created validation.js
    Created validation.js and began work on getting that set up.

---
+++ app/validation.js
@@ -0,0 +1,4 @@
+const form = document.getElementById('form')
+const username_input = document.getElementById('username-input')
+const password_input = document.getElementById('password-input')
+const confirm_password_input = document.getElementById('confirm-password-input')
